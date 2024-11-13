e5d80d24285e40611f267d43900a63a0d676e1f0
peter mumo <135279804+Mumoh254@users.noreply.github.com>
2024-11-13 12:41:42 +0300

    Add files via upload

--- arrays/script/script.js
+++ arrays/script/script.js
@@ -1,225 +1,415 @@
 
+// let  js  = "Amazing"
+// let  Pi  =  3.14
+// const   pi  =  3.14
 
-let  arr  =  []
+// const  $name =  "peter" 
+// let   _firstName =  "Elon"
+// let  person3 = "Wellness"
 
-let  magari  = [ "merceedes"  ,  "bmw",  "Subaru"  , "Range  rover" ]
+// let jinaKwanza  =  "Mariyan";
+// let Jinakwanza =  "MARIANNN"
 
-
-magari[0]   =  "Porche   Cayyene";
-console.log(magari[0])
-
-console.log(magari.length)
-
-let mchanganyiko = [
-    "subaru",
-    { jina: "Wanambuko" },
-    true,
-    function() { alert("hello"); }
-  ];
+// let   age ;
+// if  (age  ==  null)  {
+//     console.log({
+//         message: "null  value "
+//     }) 
+    
+//   age  =  40; 
   
-  alert(mchanganyiko[1].jina); 
+// }    else {
+//     console.log({
+//         message: "  value   defined "
+//     })
+// }
+// alert  (age)
 
-  mchanganyiko[3]();          
+
+// let  jina =  'john'
+// alert ( `Hellow my  name  is  ${jina}`);
+// alert(`the   result   is ${1  + 3 }`);
+
+// let  kClass  =  17 ;
+// alert( kClass)
+// console.log(typeof( kClass))
+
+// let   classFca =   20.45
+// console.log(typeof( classFca))
+
+// let isGreater   =  7  >   2 
+// alert(isGreater)
+// if ( isGreater  ==  false )  {
+//     console.log(typeof(isGreater))
+// }  else   {
+//     console.log(typeof(isGreater))
+// }
+
+// let  Name =  prompt("Enter  Name ")
+// console.log(`  My   name   is   `)
+
+// let   x  =  1
+// x  =  "peter ";
+// alert(x)
+
+
+// let  ages  =  null;
+// let names;
+// console.log(typeof(names));
+
+// console.log(typeof(ages));
+// console.log(typeof(Jinakwanza));
+
+// console.log(jinaKwanza);
+// console.log(Jinakwanza );
+// console.log(person3);
+
+// console.log($name);
+// console.log(_firstName);
+// console.log(js);
+
+
+// let x   =  1  ,  y  = 3;
+// alert(y   -  x );
+
+// alert(  10  %  3)
+// alert(5  **  5) 
+
+// alert(2 + 2  + "1") 
+
+// let  s = "My " + "string";
+
+// alert(s)
+
+// let  john  = "2";
+// let  mary =  "3";
+// alert(+john + +mary)              
+
+
+// let  arr  =  []
+
+// let  magari  = [ "merceedes"  ,  "bmw",  "Subaru"  , "Range  rover" ]
+
+
+// magari[0]   =  "Porche   Cayyene";
+// console.log(magari[0])
+
+// console.log(magari.length)
+
+// let mchanganyiko = [
+//     "subaru",
+//     { jina: "Wanambuko" },
+//     true,
+//     function() { alert("hello"); }
+//   ];
+  
+//   alert(mchanganyiko[1].jina); 
+
+//   mchanganyiko[3]();          
   
 
-  let marks  =  prompt("Enter   Your  Marks ")
+//   let marks  =  prompt("Enter   Your  Marks ")
 
-  if  ( marks >=  90)   {
-    console.log({
-        message:  " Cogratulations you  scoredv   an  A "
-    })
-  }  else   {
-    console.log({
-        message: "You   failed "
-    })
-  }
-
-
-let  wanafunzi  =  ["darren"  , "millicent"  , "kelvin" , "erick"  ,"duke" ,  "chunyu" , "Peter" , "emmah" , "sophie" , "mollent"  ,  "nganga" , "daktari"]
-//length
-console.log(wanafunzi.length)
-//7th   value  
-console.log( wanafunzi[7])
+//   if  ( marks >=  90)   {
+//     console.log({
+//         message:  " Cogratulations you  scoredv   an  A "
+//     })
+//   }  else   {
+//     console.log({
+//         message: "You   failed "
+//     })
+//   }
 
 
-let  mwaka   = prompt("When   did  kenya   receive   indipendece" , "" ) 
-console.log( mwaka)
-
-if  (  mwaka ==  1963 ) 
-
-{
-
-  alert(
-     "You   are   right "
-  )
-
-  alert(
-    "Your   smart "
-  )
-
-}   else  {
-  alert(" This  year  is  Wrong ")
-}
+// let  wanafunzi  =  ["darren"  , "millicent"  , "kelvin" , "erick"  ,"duke" ,  "chunyu" , "Peter" , "emmah" , "sophie" , "mollent"  ,  "nganga" , "daktari"]
+// //length
+// console.log(wanafunzi.length)
+// //7th   value  
+// console.log( wanafunzi[7])
 
 
-let  year  =  prompt("When   did  kenya   receive   indipendece" , "" ) 
+// let  mwaka   = prompt("When   did  kenya   receive   indipendece" , "" ) 
+// console.log( mwaka)
 
-if  (  year  ==  1963) {
+// if  (  mwaka ==  1963 ) 
 
-  alert("You  are  Right")
+// {
 
-}
-else  if  (year  >   1963)  {
+//   alert(
+//      "You   are   right "
+//   )
+
+//   alert(
+//     "Your   smart "
+//   )
+
+// }   else  {
+//   alert(" This  year  is  Wrong ")
+// }
+
+
+// let  year  =  prompt("When   did  kenya   receive   indipendece" , "" ) 
+
+// if  (  year  ==  1963) {
+
+//   alert("You  are  Right")
+
+// }
+// else  if  (year  >   1963)  {
   
-  alert(" Too  late  ,  Kenya  is  alredy  free ")
+//   alert(" Too  late  ,  Kenya  is  alredy  free ")
 
-}  else  {
+// }  else  {
 
-    alert(  "The  year  is   wrong ")
+//     alert(  "The  year  is   wrong ")
 
-}
+// }
 
   
-let mwaka = prompt("Mwaka?", 18);
+// let mwaka = prompt("Mwaka?", 18);
 
-let message = (mwaka < 3) ? "Hi, baby" :
-    (mwaka >= 80) ? 
-    "Nakula pesa ya uzee" :
-    (mwaka >= 60) ? 
-    "You are a retiree" :
-    (mwaka > 18) ? 
-    "You are an adult" :
-    "You're just a minor";
+// let message = (mwaka < 3) ? "Hi, baby" :
+//     (mwaka >= 80) ? 
+//     "Nakula pesa ya uzee" :
+//     (mwaka >= 60) ? 
+//     "You are a retiree" :
+//     (mwaka > 18) ? 
+//     "You are an adult" :
+//     "You're just a minor";
 
-alert(message);
+// alert(message);
 
-let   sir   =  8
+// let   sir   =  8
 
-if   (  sir  > 10  ||  sir >  18)  {
+// if   (  sir  > 10  ||  sir >  18)  {
   
-  alert("No  clases")
+//   alert("No  clases")
 
-} 
+// } 
 
-alert(  1 ||   0)
-alert(null  ||  1)
-alert( null   ||   0  ||  1)
-alert(  1 ||  null  ||   0)
+// alert(  1 ||   0)
+// alert(null  ||  1)
+// alert( null   ||   0  ||  1)
+// alert(  1 ||  null  ||   0)
 
-let  jinaKwanza  =  "";
-let  jinaMwisho  =  "";
-let  jinaUtaani  =  "";
+// let  jinaKwanza  =  "";
+// let  jinaMwisho  =  "";
+// let  jinaUtaani  =  "";
 
-alert(jinaKwanza  ||   jinaMwisho  ||   jinaUtaani   ||   "AsiyeJulikana");
+// alert(jinaKwanza  ||   jinaMwisho  ||   jinaUtaani   ||   "AsiyeJulikana");
 
-let    dakikia  = 12
-let  saa   =  2;
+// let    dakikia  = 12
+// let  saa   =  2;
 
-if(  saa  ==   2   &&  dakika   ==  12)  {
-  alert("  thge   time  is  2:12")
-}
+// if(  saa  ==   2   &&  dakika   ==  12)  {
+//   alert("  thge   time  is  2:12")
+// }
 
-alert(1 &&  0)
-alert(1  &&   5)
-alert(null  &&  5)
-alert( 0 &&  "hello")
+// alert(1 &&  0)
+// alert(1  &&   5)
+// alert(null  &&  5)
+// alert( 0 &&  "hello")
 
-name  =  ""
-last  =  "mumo"
-if  ( name  ==  null  &&   last  ==  "mum") {
-  console.log("true")
-}  else  {
-  console.log("false ")
-}
-
-
-
-let   x  =  1;
-
-console.log(  (x >  0)   &&   alert("kubwa  kuliko  sufuria "))
-
-console.log(!true)
-console.log(!0)
-
-let  num  =  4
-if (  num  ==  !typeof(num)){
-  console.log("true")
-} 
- else   {
-  console.log("false")
-}
-
-write   an  if   condition to  check  if  age  is btwn  14  and   90  
-
-let   age  =   prompt("Enter  age ")
-
-if (  age >= 14  &&  age <=  90)  {
-  console.log({
-    message:  "You  are   allowed "
-  })
-} 
-// write   an  if   condition    that  checks   age  is not btwn 14  &&  90  inclusive
-else  {
-  console.log({
-    message:  "Acess  denied "
-  })
-}
-
-if (  age <= 14  &&  age >=  90)
-  {
-    console.log({
-      message:  "You  are   allowed "
-    })
-  } 
-  // write   an  if   condition    that  checks   age  is not btwn 14  &&  90  inclusive
-  else  {
-    console.log({
-      message:  "Acess  denied "
-    })
-  }
+// name  =  ""
+// last  =  "mumo"
+// if  ( name  ==  null  &&   last  ==  "mum") {
+//   console.log("true")
+// }  else  {
+//   console.log("false ")
+// }
 
 
-let  role  =  prompt("Enter  your role" , '')
 
-if(  role == "admin")  {
+// let   x  =  1;
 
-  let  pass =  prompt("Enter   password" , '')
+// console.log(  (x >  0)   &&   alert("kubwa  kuliko  sufuria "))
 
-  if (pass=="master") {
-   console.log({
-    message:  "your   welcome "
-   })
+// console.log(!true)
+// console.log(!0)
 
-  }   
-  else  if ( pass  ===  ""  ||  pass  === null)  {
-    console.log({
-      message:  "cancelled "
-    })
-  }  else  {
-    console.log({
-      message: "Wrong  password "
-    })
+// let  num  =  4
+// if (  num  ==  !typeof(num)){
+//   console.log("true")
+// } 
+//  else   {
+//   console.log("false")
+// }
+
+// write   an  if   condition to  check  if  age  is btwn  14  and   90  
+
+// let   age  =   prompt("Enter  age ")
+
+// if (  age >= 14  &&  age <=  90)  {
+//   console.log({
+//     message:  "You  are   allowed "
+//   })
+// } 
+// // write   an  if   condition    that  checks   age  is not btwn 14  &&  90  inclusive
+// else  {
+//   console.log({
+//     message:  "Acess  denied "
+//   })
+// }
+
+// if (  age <= 14  &&  age >=  90)
+//   {
+//     console.log({
+//       message:  "You  are   allowed "
+//     })
+//   } 
+//   // write   an  if   condition    that  checks   age  is not btwn 14  &&  90  inclusive
+//   else  {
+//     console.log({
+//       message:  "Acess  denied "
+//     })
+//   }
+
+
+// let  role  =  prompt("Enter  your role" , '')
+
+// if(  role == "admin")  {
+
+//   let  pass =  prompt("Enter   password" , '')
+
+//   if (pass=="master") {
+//    console.log({
+//     message:  "your   welcome "
+//    })
+
+//   }   
+//   else  if ( pass  ===  ""  ||  pass  === null)  {
+//     console.log({
+//       message:  "cancelled "
+//     })
+//   }  else  {
+//     console.log({
+//       message: "Wrong  password "
+//     })
   
-  }
+//   }
  
-}  else if ( role  ==  "" )  {
-  console.log({message: "Cancelled" })
+// }  else if ( role  ==  "" )  {
+//   console.log({message: "Cancelled" })
 
-}  else if (  role !=  "admin" )  {
- console.log({ message: "i  dont  know  you "})
-}
+// }  else if (  role !=  "admin" )  {
+//  console.log({ message: "i  dont  know  you "})
+// }
 
 
-two  op   nulish  col  oparater  turns value if  on left  is  null  0r  undefined 
+// two  op   nulish  col  oparater  turns value if  on left  is  null  0r  undefined 
 
-a??b
+// a??b
 
-let   x  =  1
+// let   x  =  1
 
-alert(x??"haijulikani")
+// alert(x??"haijulikani")
 
-let  jinaKwanza =  null ;
-let  jinaPili  =  "Mumo";
-let  jinaTatu   =  null;
+// let  jinaKwanza =  null ;
+// let  jinaPili  =  "Mumo";
+// let  jinaTatu   =  null;
 
-alert(jinaKwanza??jinaPili??jinaTatu??"Haijulikani")
+// alert(jinaKwanza??jinaPili??jinaTatu??"Haijulikani")
+
+// while(Condition) {
+
+// }
+
+console.log("while  loop")
+// let  a  =  3;
+// while (a>0) {
+
+// }
+
+// let i  = 0;
+// while(i<5) {
+
+//   alert(i++);
+  
+// }
+// let i  = 10;
+// while  (i >  5)  {
+//   alert(i--)
+// }
+
+// let i  = 10;
+// while(i!=0)  alert(i--)
+
+// let i  = 10;
+//   while ( 1 <  11)  {
+//     alert( i -= 2)
+//   }
+
+  console.log("doWhile()")
+
+  // let  a  = 0;
+
+//icrement  
+  // do {
+  //   alert(a++)
+  // }
+  // while (a < 5 );
+
+  //decrement 
+  // let  a  = 0;
+  // do {
+  //   alert(a--)
+  // }
+  // while (a < 5 );
+
+  //increment    by   two 
+  // let  a  = 0;
+  // do {
+  //   alert(a +=  2)
+  // }
+  // while (a < 5 );
+
+ 
+  // for(let b = 0; b < 5 ; b++){
+  //   alert(b)
+  // }
+
+  
+  // for(let   c =  0; c < 20  ; c++){
+
+  //  if(c ==  13)  {
+   
+  //     break;
+  //  } else {
+  //   console.log(c)
+  //  }
+
+  // }
+
+// let  sum  =  0;
+
+// while(true){
+//  let  value  = +prompt("enter  number " ,  "");
+
+//  if( !value ) break;
+
+//   sum+=value;
+
+// } 
+// alert(`sum of  the  numbers  u   eneterd  are${sum}`);
+
+//odd numbers    btwn  0   and   20 
+  // 1  2   3  4   5   6   7   8   9   10 
+
+// for(let odd  =  0  ; odd <= 3 ; odd++){
+
+//   //even  num  divisible  by  2
+//   if(odd%2){
+//    console.log("i  am  an  even  number ");
+//   }  else  {
+//     continue
+//   }
+//     console.log(odd);
+// } 
+
+//write   a  loop   which  prompts  for   a  number > 1000 
+//if  the  user   enter   anothe r  number  ask  them   to  input   again 
+
+
+//task  that   prints  a  prime  num,ber  fom  0 to  17  prime  number  shld  be  in  an  interval  of   two
+
+
